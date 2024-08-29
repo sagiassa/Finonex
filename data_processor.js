06a3a3cdd2bb331d2c844817557fef491cfce173
sagiassa <assasagi@gmail.com>
2024-08-29 11:41:46 +0300

    added a getcreate function for db

--- data_processor.js
+++ data_processor.js
@@ -5,26 +5,42 @@ const csv = require('csv-parser')
 let users = [];
 let users_events = [];
 let usersRevenue = {}
-// PostgreSQL connection setup
+
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'postgres',
-    password: 'theassa1504', //TODO remove it 
+    password: '****', //TODO enter your own password
     port: 5432,
 });
 
 const db_table = fs.readFileSync('./db.sql', 'utf8')
 
+// A functions that check if the revenue table is already created ot needed to be created
+async function getOrCreateUserRevenue() {
+    try {
+        await pool.query('SELECT * FROM users_revenue');
+        console.log('Table ensured.');
+    } catch (err) {
+        console.log('Table was not created yet.');
+        await pool.query(db_table)
+        console.log('created users revnue table.')
+    }
+}
+
+// A function that returns the amount that needs to be added\subtract based on the action name
 function getAmount(name, value) {
     switch (name) {
         case "add_revenue":
             return value;
         case "subtract_revenue":
             return -1 * value;
+        default:
+            throw Error('Unvalid action')
     }
 }
 
+// A function that upserts the amount to the user's revenue.
 async function updateRevenue({ userId, value }) {
     const query = 'SELECT update_revenue($1, $2)';
     const values = [userId, value];
@@ -35,6 +51,8 @@ async function updateRevenue({ userId, value }) {
         console.error('Error updating revenue', err.stack);
     }
 }
+
+// A function that fetches the data from the local CSV
 async function fetchDataFromCSV() {
     return new Promise((resolve, reject) => {
         fs.createReadStream('./events_data.csv')
@@ -45,6 +63,7 @@ async function fetchDataFromCSV() {
     });
 }
 
+// A function that calculates user's transactions based on the events that were added by the client
 async function calculateUserRevenue() {
     users = [...new Set(users_events.map(event => event.userId))]
     console.log(users)
@@ -66,9 +85,9 @@ async function calculateUserRevenue() {
 }
 
 (async = async () => {
-    // await pool.query(db_table)
-    await fetchDataFromCSV()
-    await calculateUserRevenue()
+    await getOrCreateUserRevenue();
+    await fetchDataFromCSV();
+    await calculateUserRevenue();
     await eachOfLimit(users, 1, async (user, key) => {
         try {
             await updateRevenue(usersRevenue[user]);
